f37d10a13dad10caca6868dff3509d4d3a6547a9
Mikhail Metrikin <metrikin@gmail.com>
2017-05-15 21:50:01 -0700

    Sync changed to force:false

--- app-events/chat.js
+++ app-events/chat.js
@@ -3,10 +3,7 @@ module.exports = function(io){
   io.on('connection', function(socket){
     // User connect event logic here
 
-    console.log('a user connected');
-
     socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
       io.emit('chat message', msg);
     });
 
