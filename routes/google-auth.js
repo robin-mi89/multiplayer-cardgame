ddb21af0c96d339228d04a289bb7241d0040c1b2
Robin Mi <si.mi5689@gmail.com>
2017-05-16 16:51:23 -0700

    finished debugging passport.js, now all req has a req.user object that keeps track of user

--- routes/google-auth.js
+++ routes/google-auth.js
@@ -3,19 +3,36 @@ var router = express.Router();
 var passport = require('passport');
 require('../config/passport')(passport); 
 
-router.get('', passport.authenticate('google', { scope : ['profile', 'email'] }));
-router.get('/callback',
-            passport.authenticate('google', {
-                successRedirect : '/profile',
-                failureRedirect : '/'
+router.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+router.get('/auth/google/callback',
+            passport.authenticate('google', 
+            {
+                successRedirect : '/',
+                failureRedirect : '/',
+                function(req, res)
+                {
+                    console.log(req.session);
+                }
             }));
 
-function isLoggedIn(req, res, next) {
-    console.log("checking logged in");
-    if (req.isAuthenticated())
-        return next();
+router.get('/api/user', function(req, res)
+{
+    console.log(req.session);
+    console.log(req.user);
+    res.json(req.user);
+})
 
-    res.redirect('/auth/google');
-}
+router.get('/logout', function(req, res) 
+{
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/api/user', function(req, res)
+{
+    console.log(req.session);
+    res.json({user: req.user});
+});
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+require('../config/passport')(passport); 
 
 /* GET START home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Memeski' });
-});
 
+
+router.get('/', ensureAuthenticated, function(req, res)
+{
+  if(req.user)
+  {
+    console.log(req.user);
+    res.render('index', 
+  { 
+    title: 'Memeski',
+    user: user 
+  });
+  }
+})
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    else{
+        res.redirect('/auth/google')
+    }
+}
 module.exports = router;
