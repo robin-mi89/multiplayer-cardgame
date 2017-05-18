ebb73bb1ab8f1044439fb6b10d72faf39733da2c
Mikhail Metrikin <metrikin@gmail.com>
2017-05-17 23:26:31 -0700

    Cleaning up code before master merge

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,4 @@ var router = express.Router();
 var passport = require('passport');
 require('../config/passport')(passport);
 
-// router.get('/', ensureAuthenticated, function(req, res)
-// {
-//   if(req.user)
-//   {
-//     console.log(req.user);
-//     res.render('index',
-//   {
-//     title: 'Memeski',
-//     user: user
-//   });
-//   }
-// });
-//
-// function ensureAuthenticated(req, res, next) {
-//
-//   if (req.isAuthenticated())
-//         return next();
-//     else{
-//         res.redirect('/auth/google')
-//     }
-// }
 module.exports = router;
--- seeders/createData.js
+++ seeders/createData.js
@@ -2,16 +2,6 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
 
     return queryInterface.bulkInsert('Memes', [
       {"name":"One Does Not Simply","url":"https://i.imgflip.com/1bij.jpg"},
