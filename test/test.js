c5331ec3e8a8468ca0edff75adbab9d77ee10096
Robin Mi <si.mi5689@gmail.com>
2017-05-19 00:23:22 -0700

    test now works?

--- test/test.js
+++ test/test.js
@@ -1,22 +1,23 @@
 "use strict";
 
 const request = require('supertest');
-const app = require('../bin/www');
+const app = require('../app.js');
+const server = require('../bin/www');
 
 describe('GET /', function() {
-  it('loads web page', function(done) {
-    request(app.server)
+  it('loads web page, and then redirects if not logged in', function(done) {
+    request(server)
       .get('/')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
+      .expect('Content-Type', 'text/plain; charset=utf-8')
+      .expect(302, done);
   });
 });
 
 describe('GET /auth/google', function() {
-  it('loads web page', function(done) {
-    request(app.server)
+  it('loads web page, then redirects', function(done) {
+    request(server)
       .get('/auth/google')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
+      .expect(302, done);
   });
 });
+
