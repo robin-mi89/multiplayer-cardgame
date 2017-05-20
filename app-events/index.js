54e588b216a413948487cf4e53ca36b5d7b5d1c3
Mikhail Metrikin <metrikin@gmail.com>
2017-05-19 23:47:18 -0700

    WIP circular route

--- app-events/index.js
+++ app-events/index.js
@@ -69,6 +69,7 @@ module.exports = function(io, db) {
 
         if (roundSubs === 4) {
           // All four things submitted
+          roundSubs = 0;
           clearInterval(roundInterval);
           io.emit('judgment round')
         }
